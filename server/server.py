b8653c3e8648067a19f24d7203614d4b4b269ed1
John Carse <jtcarse@gmail.com>
2019-05-16 14:31:35 -0500

    example app now working

--- server/server.py
+++ server/server.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
 from flask_cors import cross_origin
+from pymongo import MongoClient
+from datetime import datetime
 
 app = Flask(__name__)
+client = MongoClient('mongo', 27017)
+db = client.db
 
 @app.route('/', methods=['GET'])
 @cross_origin()
 def root():
-	return jsonify({ 'message': u'Hello from ReAsk!' })
+	db.hits.insert_one({ 'time': datetime.utcnow() })
+	message = 'This page has been visited {} times.'.format(db.hits.count())
+	return jsonify({ 'message': message })
 
 if __name__ == '__main__':
 	# only used locally
